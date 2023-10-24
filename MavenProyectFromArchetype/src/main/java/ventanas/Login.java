c47dc6226729e94c12e460eccf695ec3876a7350
Character25 <vicentemt01@gmail.com>
2023-10-24 14:17:02 +0200

    Login_&_Registro_mejorado

--- MavenProyectFromArchetype/src/main/java/ventanas/Login.java
+++ MavenProyectFromArchetype/src/main/java/ventanas/Login.java
@@ -19,10 +19,18 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.Color;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.ImageIcon;
+import java.awt.Component;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.LineBorder;
 
 public class Login {
 
-	private JFrame frame;
+	private JFrame frmLogin;
 	private JTextField txtUsuario;
 	private JTextField txtContrasea;
 
@@ -33,9 +41,9 @@ public class Login {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UIManager.setLookAndFeel("com.jtattoo.plaf.bernstein.BernsteinLookAndFeel");
+					UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
 					Login window = new Login();
-					window.frame.setVisible(true);
+					window.frmLogin.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -54,77 +62,120 @@ public class Login {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame("AppMusic");
-		frame.setVisible(true);
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		frmLogin = new JFrame("AppMusic");
+		frmLogin.setTitle("Login");
+		frmLogin.setVisible(true);
+		frmLogin.setBounds(100, 100, 400, 250);
+		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLogin.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.SOUTH);
+		JPanel Bottons_panel = new JPanel();
+		frmLogin.getContentPane().add(Bottons_panel, BorderLayout.SOUTH);
 		
 		JButton btnNewButton = new JButton("Login");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton.setIcon(new ImageIcon(Login.class.getResource("/recursos/usuario.png")));
+		btnNewButton.addActionListener(ev -> {
+			
 		});
-		panel.add(btnNewButton);
+		Bottons_panel.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Login con GitHub");
-		panel.add(btnNewButton_1);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton_1.setIcon(new ImageIcon(Login.class.getResource("/recursos/github.png")));
+		Bottons_panel.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Registro");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
+		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton_2.setIcon(new ImageIcon(Login.class.getResource("/recursos/editar.png")));
+		btnNewButton_2.addActionListener(ev -> {
+			// Crea y muestra la VentanaRegistro cuando se presiona el botón
+			Registro VentanaRegistro = new Registro();
+            VentanaRegistro.setVisible(true);
+            frmLogin.setVisible(false);
 		});
-		panel.add(btnNewButton_2);
+		Bottons_panel.add(btnNewButton_2);
 		
-		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
+		JPanel Title_panel = new JPanel();
+		Title_panel.setBorder(null);
+		frmLogin.getContentPane().add(Title_panel, BorderLayout.NORTH);
+		GridBagLayout gbl_Title_panel = new GridBagLayout();
+		gbl_Title_panel.columnWidths = new int[]{0, 175, 0, 0};
+		gbl_Title_panel.rowHeights = new int[]{51, 0, 0};
+		gbl_Title_panel.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_Title_panel.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		Title_panel.setLayout(gbl_Title_panel);
+		
+		JPanel Space_panel_1 = new JPanel();
+		GridBagConstraints gbc_Space_panel_1 = new GridBagConstraints();
+		gbc_Space_panel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_Space_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_Space_panel_1.gridx = 0;
+		gbc_Space_panel_1.gridy = 0;
+		Title_panel.add(Space_panel_1, gbc_Space_panel_1);
+		
+		JPanel Name_panel = new JPanel();
+		Name_panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		GridBagConstraints gbc_Name_panel = new GridBagConstraints();
+		gbc_Name_panel.insets = new Insets(0, 0, 5, 5);
+		gbc_Name_panel.fill = GridBagConstraints.BOTH;
+		gbc_Name_panel.gridx = 1;
+		gbc_Name_panel.gridy = 0;
+		Title_panel.add(Name_panel, gbc_Name_panel);
 		
 		JLabel lblNewLabel = new JLabel("AppMusic");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		panel_1.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Source Sans Pro Black", Font.PLAIN, 40));
+		Name_panel.add(lblNewLabel);
 		
-		JPanel panel_2 = new JPanel();
-		frame.getContentPane().add(panel_2, BorderLayout.CENTER);
-		GridBagLayout gbl_panel_2 = new GridBagLayout();
-		gbl_panel_2.columnWidths = new int[]{50, 80, 0, 200, 50, 0};
-		gbl_panel_2.rowHeights = new int[]{20, 35, 20, 35, 20, 0};
-		gbl_panel_2.columnWeights = new double[]{1.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_2.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		panel_2.setLayout(gbl_panel_2);
+		JPanel Space_panel_2 = new JPanel();
+		GridBagConstraints gbc_Space_panel_2 = new GridBagConstraints();
+		gbc_Space_panel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_Space_panel_2.fill = GridBagConstraints.BOTH;
+		gbc_Space_panel_2.gridx = 2;
+		gbc_Space_panel_2.gridy = 0;
+		Title_panel.add(Space_panel_2, gbc_Space_panel_2);
+		
+		JPanel Text_panel = new JPanel();
+		Text_panel.setBorder(null);
+		frmLogin.getContentPane().add(Text_panel, BorderLayout.CENTER);
+		GridBagLayout gbl_Text_panel = new GridBagLayout();
+		gbl_Text_panel.columnWidths = new int[]{50, 80, 0, 200, 50, 0};
+		gbl_Text_panel.rowHeights = new int[]{15, 35, 15, 35, 15, 0};
+		gbl_Text_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_Text_panel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		Text_panel.setLayout(gbl_Text_panel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Usuario:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblNewLabel_1.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_1.gridx = 1;
 		gbc_lblNewLabel_1.gridy = 1;
-		panel_2.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		Text_panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		txtUsuario = new JTextField();
+		txtUsuario.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtUsuario.setText("Usuario");
 		GridBagConstraints gbc_txtUsuario = new GridBagConstraints();
 		gbc_txtUsuario.fill = GridBagConstraints.BOTH;
 		gbc_txtUsuario.insets = new Insets(0, 0, 5, 5);
 		gbc_txtUsuario.gridx = 3;
 		gbc_txtUsuario.gridy = 1;
-		panel_2.add(txtUsuario, gbc_txtUsuario);
+		Text_panel.add(txtUsuario, gbc_txtUsuario);
 		txtUsuario.setColumns(4);
 		
 		JLabel lblNewLabel_2 = new JLabel("Contraseña:");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblNewLabel_2.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_2.gridx = 1;
 		gbc_lblNewLabel_2.gridy = 3;
-		panel_2.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		Text_panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
 		txtContrasea = new JTextField();
+		txtContrasea.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtContrasea.setSelectedTextColor(new Color(255, 255, 255));
 		txtContrasea.setText("Contraseña");
 		GridBagConstraints gbc_txtContrasea = new GridBagConstraints();
@@ -132,20 +183,13 @@ public class Login {
 		gbc_txtContrasea.fill = GridBagConstraints.BOTH;
 		gbc_txtContrasea.gridx = 3;
 		gbc_txtContrasea.gridy = 3;
-		panel_2.add(txtContrasea, gbc_txtContrasea);
+		Text_panel.add(txtContrasea, gbc_txtContrasea);
 		txtContrasea.setColumns(10);
 		
-		btnNewButton_2.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent e) {
-	                Registro ventana2 = new Registro();
-	                ventana2.setVisible(true);
-	                frame.setVisible(false);
-	            }
-	        });
 	}
 	
 	public void setVisible() {
-		frame.setVisible(true);
+		frmLogin.setVisible(true);
 	}
 
 }
--- MavenProyectFromArchetype/src/main/java/ventanas/Registro.java
+++ MavenProyectFromArchetype/src/main/java/ventanas/Registro.java
@@ -9,6 +9,8 @@ import javax.swing.JButton;
 import javax.swing.BoxLayout;
 import java.awt.FlowLayout;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
@@ -23,6 +25,7 @@ import javax.swing.ImageIcon;
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.JPasswordField;
+import java.awt.Font;
 
 public class Registro extends JFrame {
 
@@ -48,6 +51,7 @@ public class Registro extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
 					Registro frame = new Registro();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -61,8 +65,9 @@ public class Registro extends JFrame {
 	 * Create the frame.
 	 */
 	public Registro() {
+		setTitle("Registro");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 350);
+		setBounds(100, 100, 550, 250);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -75,18 +80,17 @@ public class Registro extends JFrame {
 		
 		btnNewButton_1 = new JButton("Volver");
 		panel.add(btnNewButton_1);
-		btnNewButton_1.setIcon(new ImageIcon(Registro.class.getResource("/recursos/volver (2).png")));
+		btnNewButton_1.setIcon(new ImageIcon(Registro.class.getResource("/recursos/flecha-hacia-atras.png")));
 		
-		btnNewButton_1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Crea y muestra la Ventana2 cuando se presiona el botón
-                Login ventana2 = new Login();
-                ventana2.setVisible();
-                setVisible(false);
-            }
-        });
+		btnNewButton_1.addActionListener(ev -> {
+			// Crea y muestra la VentanaLogin cuando se presiona el botón
+			Login VentanaLogin = new Login();
+			VentanaLogin.setVisible();
+			setVisible(false);
+		});
 		
 		btnNewButton = new JButton("Registro");
+		btnNewButton.setIcon(new ImageIcon(Registro.class.getResource("/recursos/anadir.png")));
 		panel.add(btnNewButton);
 		
 		panel_1 = new JPanel();
@@ -94,11 +98,12 @@ public class Registro extends JFrame {
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
 		gbl_panel_1.columnWidths = new int[]{15, 100, 100, 100, 100, 15, 0};
 		gbl_panel_1.rowHeights = new int[]{15, 0, 15, 0, 15, 0, 15, 0};
-		gbl_panel_1.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWeights = new double[]{1.0, 0.0, 1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
 		gbl_panel_1.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
 		
 		lblNewLabel_1 = new JLabel("Usuario:");
+		lblNewLabel_1.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
@@ -107,6 +112,7 @@ public class Registro extends JFrame {
 		panel_1.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		txtUsuario = new JTextField();
+		txtUsuario.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtUsuario.setText("usuario");
 		GridBagConstraints gbc_txtUsuario = new GridBagConstraints();
 		gbc_txtUsuario.insets = new Insets(0, 0, 5, 5);
@@ -117,6 +123,7 @@ public class Registro extends JFrame {
 		txtUsuario.setColumns(10);
 		
 		lblNewLabel_3 = new JLabel("Contraseña:");
+		lblNewLabel_3.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
 		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
@@ -125,6 +132,7 @@ public class Registro extends JFrame {
 		panel_1.add(lblNewLabel_3, gbc_lblNewLabel_3);
 		
 		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		GridBagConstraints gbc_passwordField = new GridBagConstraints();
 		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
 		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
@@ -133,6 +141,7 @@ public class Registro extends JFrame {
 		panel_1.add(passwordField, gbc_passwordField);
 		
 		lblNewLabel_2 = new JLabel("Nombre Completo:");
+		lblNewLabel_2.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
@@ -141,6 +150,7 @@ public class Registro extends JFrame {
 		panel_1.add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
 		txtNombreCompleto = new JTextField();
+		txtNombreCompleto.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtNombreCompleto.setText("nombre completo");
 		txtNombreCompleto.setHorizontalAlignment(SwingConstants.LEFT);
 		txtNombreCompleto.setToolTipText("");
@@ -154,6 +164,7 @@ public class Registro extends JFrame {
 		txtNombreCompleto.setColumns(10);
 		
 		lblNewLabel = new JLabel("Fecha Nacimiento:");
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
