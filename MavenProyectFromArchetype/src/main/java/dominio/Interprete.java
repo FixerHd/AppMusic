b0e66f32c54e150665dccca9b036528f7b3604b5
FixerHd <fixerhd03@gmail.com>
2023-11-27 17:45:39 +0100

    amai

--- MavenProyectFromArchetype/src/main/java/dominio/Interprete.java
+++ MavenProyectFromArchetype/src/main/java/dominio/Interprete.java
@@ -1,5 +1,17 @@
 package dominio;
 
+import java.util.HashSet;
+
 public class Interprete {
+	
+	private String nombre;
+	
+	public Interprete(String name) {
+		nombre=name;
+	}
+	
+	public HashSet<Cancion> getCanciones() {
+		return AppMusic.getCancionesInterprete(this);
+	}
 
 }
--- MavenProyectFromArchetype/src/main/java/dominio/Usuario.java
+++ MavenProyectFromArchetype/src/main/java/dominio/Usuario.java
@@ -3,10 +3,11 @@ package dominio;
 import java.util.HashSet;
 
 public class Usuario {
+	private int id;
     private String nombre;
     private String email;
-    private String contraseña;
-    private String fechanac;
+    private String password;
+    private String fechaNacimiento;
     private boolean premium;
     private HashSet<Playlist> playlists;
     private Descuento desc;
@@ -15,8 +16,8 @@ public class Usuario {
     public Usuario(String nombre, String email, String contraseña, String fechanac) {
         this.nombre = nombre;
         this.email = email;
-        this.contraseña = contraseña;
-        this.fechanac = fechanac;
+        this.password = contraseña;
+        this.fechaNacimiento = fechanac;
         premium=false;
         playlists=new HashSet<Playlist>();
     }
@@ -29,6 +30,51 @@ public class Usuario {
     public void addPlaylist(Playlist p) {
     	playlists.add(p);
     }
+
+
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String getPlayliststoEntity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String getFechaNacimiento() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String getPremium() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 
 }
