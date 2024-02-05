79e0288324dd2a07a229c985c5e61190e5458fce
Character25 <vicentemt01@gmail.com>
2024-02-05 13:08:56 +0100

    Little_change

--- MavenProyectFromArchetype/src/main/java/ventanas/Lanzador.java
+++ MavenProyectFromArchetype/src/main/java/ventanas/Lanzador.java
@@ -7,8 +7,6 @@ import Controlador.AppMusic;
 
 public class Lanzador {
 
-	private static final String LAF = "com.jtattoo.plaf.texture.TextureLookAndFeel";
-
 	public static void main(String[] args) {
 
 		AppMusic.getUnicaInstancia().setEstilo(AppMusic.getEstilo());
