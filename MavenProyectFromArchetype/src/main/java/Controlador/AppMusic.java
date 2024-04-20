3b713f6ebaeb12ab59e33c78fc9536721c99a2bd
FixerHD <fixerhd03@gmail.com>
2024-04-20 20:27:32 +0200

    amador

--- MavenProyectFromArchetype/src/main/java/Controlador/AppMusic.java
+++ MavenProyectFromArchetype/src/main/java/Controlador/AppMusic.java
@@ -34,7 +34,9 @@ import java.io.FileOutputStream;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 public class AppMusic {
 
