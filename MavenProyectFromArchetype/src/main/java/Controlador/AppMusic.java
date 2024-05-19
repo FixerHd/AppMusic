8a89ce108633c857fb2dc38263856a75d996fcf1
FixerHD <fixerhd03@gmail.com>
2024-05-19 16:01:35 +0200

    pdf

--- MavenProyectFromArchetype/src/main/java/Controlador/AppMusic.java
+++ MavenProyectFromArchetype/src/main/java/Controlador/AppMusic.java
@@ -23,6 +23,7 @@ import dominio.Playlist;
 import dominio.Reproductor;
 import dominio.CatalogoCanciones;
 import dominio.CatalogoUsuarios;
+import dominio.CreadorPDF;
 import dominio.DatosLista;
 import dominio.DatosTabla;
 import dominio.Descuento;
@@ -312,57 +313,7 @@ public class AppMusic {
 	 * @return true if the PDF document is created successfully, false otherwise.
 	 */
 	public boolean crearPDF() {
-		try {
-			// Obtener el nombre del usuario activo
-			String nombreUsuario = usuarioActivo.getNombre();
-
-			// Crear el documento PDF
-			Document document = new Document();
-			PdfWriter.getInstance(document, new FileOutputStream(nombreUsuario + ".pdf"));
-			document.open();
-
-			// Agregar el título del documento
-			Paragraph titulo = new Paragraph("Playlists de " + nombreUsuario);
-			titulo.setAlignment(Element.ALIGN_CENTER);
-			titulo.setSpacingAfter(20);
-			document.add(titulo);
-
-			// Recorrer las playlists del usuario activo
-			for (Playlist playlist : usuarioActivo.getPlaylists()) {
-				// Agregar el nombre de la playlist
-				Paragraph nombrePlaylist = new Paragraph("Playlist: " + playlist.getNombre());
-				nombrePlaylist.setSpacingAfter(10);
-				document.add(nombrePlaylist);
-
-				// Crear una tabla para mostrar las canciones de la playlist
-				PdfPTable tablaCanciones = new PdfPTable(3);
-				tablaCanciones.setWidthPercentage(100);
-				tablaCanciones.setSpacingAfter(10);
-
-				// Agregar encabezados de columna
-				tablaCanciones.addCell("Título");
-				tablaCanciones.addCell("Intérprete");
-				tablaCanciones.addCell("Estilo");
-
-				// Recorrer las canciones de la playlist
-				for (Cancion cancion : playlist.getCanciones()) {
-					tablaCanciones.addCell(cancion.getTitulo());
-					tablaCanciones.addCell(cancion.getInterprete());
-					tablaCanciones.addCell(cancion.getEstilomusical());
-				}
-
-				// Agregar la tabla de canciones al documento
-				document.add(tablaCanciones);
-			}
-
-			// Cerrar el documento
-			document.close();
-
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+		return CreadorPDF.getUnicaInstancia().crearPDF(usuarioActivo);
 	}
 
 	/**
