11e9099ee497a10d7d388a1a6f3044b7f33317b3
FixerHD <fixerhd03@gmail.com>
2024-06-10 00:28:06 +0200

    Revert "yes"
    This reverts commit 29c6ca9691fe97938eb47b28e99e14abedfb6f73.

--- MavenProyectFromArchetype/src/main/java/persistencia/AdaptadorCancionTDS.java
+++ MavenProyectFromArchetype/src/main/java/persistencia/AdaptadorCancionTDS.java
@@ -49,6 +49,9 @@ public class AdaptadorCancionTDS implements IAdaptadorCancionDAO {
 		// asignar identificador unico
 		// Se aprovecha el que genera el servicio de persistencia
 		Cancion.setId(eCancion.getId());
+		
+		System.out.println(eCancion.getNombre()+ " fart");
+		System.out.println(eCancion.getId());
 	}
 
 	public void borrarCancion(Cancion Cancion) {
